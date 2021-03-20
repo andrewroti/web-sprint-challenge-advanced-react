f49f3e33244c34ec4e9af4f051c98830a9d3f31b
andrewroti <andrewroti@gmail.com>
2021-03-20 03:58:09 -0400

    made custom hook

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
+import UserInput  from '../hooks/useForm.js'
 
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+// const initialValue = {
+//   firstName: "",
+//   lastName: "",
+//   address: "",
+//   city: "",
+//   state: "",
+//   zip: "",
+// };
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
+const CheckoutForm = () => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [values, setValues] = useState(initialValue);
+  const [firstName, setFirstName, handleFirstName] = UserInput('')
+  const [lastName, setLastName, handleLastName] =UserInput('')
+  const [address, setAddress, handleAddress]=UserInput('')
+  const [city, setCity, handleCity]=UserInput('');
+  const [state, setState, handleState]=UserInput('');
+  const [zip, setZip, handleZip]=UserInput('')
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowSuccessMessage(true);
+    // setFirstName('');
+    // setLastName('');
+    // setAddress('');
+    // setCity('');
+    // setState('');
+    // setZip('');
   };
-
+  
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -34,37 +47,37 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
-            value={values.firstName}
-            onChange={handleChanges}
+            value={firstName}
+            onChange={e=>handleFirstName(e.target.value)}
           />
         </label>
         <label>
           Last Name:
           <input
             name="lastName"
-            value={values.lastName}
-            onChange={handleChanges}
+            value={lastName}
+            onChange={e=>handleLastName(e.target.value)}
           />
         </label>
         <label>
           Address:
           <input
             name="address"
-            value={values.address}
-            onChange={handleChanges}
+            value={address}
+            onChange={e=>handleAddress(e.target.value)}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={city} onChange={e=>handleCity(e.target.value)} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={state} onChange={e=>handleState(e.target.value)} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={zip} onChange={e=>handleZip(e.target.value)} />
         </label>
         <button>Checkout</button>
       </form>
@@ -78,11 +91,11 @@ const CheckoutForm = (props) => {
           <br />
           <br />
           <p>
-            {values.firstName} {values.lastName}
+            {firstName} {lastName}
           </p>
-          <p>{values.address}</p>
+          <p>{address}</p>
           <p>
-            {values.city}, {values.state} {values.zip}
+            {city}, {state} {zip}
           </p>
         </div>
       )}
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+function UserInput(){
+    const [value, setValue] = useState('');
+    const handleChanges = newValue =>{
+        setValue(newValue);
+    };
+    return [value, setValue, handleChanges];
+};
+
+export default UserInput;
+
+
+
+
