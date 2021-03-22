b198f467693f31af14428bf78b247d052f2ec5dc
andrewroti <andrewroti@gmail.com>
2021-03-22 02:51:38 -0400

    wrap up

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -11,4 +12,12 @@ test("form header renders", () => {
     expect(element).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+    const form = render(<CheckoutForm/>);
+    const successMessage = form.queryByText(/You have ordered some plants! Woo-hoo!/i);
+    const button = form.queryByText(/CHECKOUT/);
+    userEvent.click(button);
+    expect(successMessage).toBeInTheDocument();
+
+});
